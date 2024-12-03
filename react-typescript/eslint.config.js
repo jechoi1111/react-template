ff891ffb5885d1fcdea190578fcb7d8359b62e61
jechoi1111 <jechoi1111@gmail.com>
2024-12-03 23:45:06 +0900

    feat: eslint 설정 추가 및 적용

--- react-typescript/eslint.config.js
+++ react-typescript/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
+import perfectionist from 'eslint-plugin-perfectionist';
 
 export default tseslint.config(
   { ignores: ['dist', 'node_modules', 'public', '.prettierrc'] },
@@ -28,6 +30,7 @@ export default tseslint.config(
     rules: {
       'no-restricted-syntax': [
         'error',
+        // type 키워드를 포함한 import는 import type 형식을 강제
         {
           selector: "ImportDeclaration ImportSpecifier[importKind='type']",
           message:
@@ -39,12 +42,12 @@ export default tseslint.config(
   {
     plugins: tseslint.plugin,
     rules: {
-      '@typescript-eslint/array-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-function-type': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/array-type': 'error', // 배열 선언 방식 명확하게
+      '@typescript-eslint/no-explicit-any': 'error', // any 타입 사용 X
+      '@typescript-eslint/no-unsafe-function-type': 'off', // 안전하지 않은 함수 타입 사용 허용
+      '@typescript-eslint/no-empty-object-type': 'off', // 빈 객체 타입 허용 => {}
+      '@typescript-eslint/consistent-type-imports': 'error', // type import 강제
+      '@typescript-eslint/no-unused-expressions': 'off', // 사용되지 않는 표현식 사용 허용
       '@typescript-eslint/naming-convention': [
         'error',
         // 클래스, 인터페이스, 타입 별칭, enum은 PascalCase
@@ -55,5 +58,45 @@ export default tseslint.config(
       ],
     },
   },
-  eslintConfigPrettier,
+  eslintConfigPrettier, // Prettier와 ESLint 규칙 충돌 방지
+  {
+    plugins: { import: eslintPluginImport },
+    rules: {
+      'import/no-duplicates': 'error', // 중복된 import 금지
+    },
+  },
+  {
+    plugins: { perfectionist },
+    rules: {
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          internalPattern: ['@/**'], // 내부 경로 패턴
+          newlinesBetween: 'always', // 그룹 간 빈 줄 추가
+          groups: [
+            'react',
+            'type',
+            ['builtin', 'external'],
+            'internal-type',
+            'internal',
+            ['parent-type', 'sibling-type', 'index-type'],
+            ['parent', 'sibling', 'index'],
+            'object',
+            'unknown',
+          ],
+          customGroups: {
+            value: {
+              react: ['react', 'react-*'],
+            },
+            type: {
+              react: ['react', 'react-*'],
+            },
+          },
+        },
+      ],
+      'perfectionist/sort-named-imports': 'error', // 이름 지정 import 정렬
+      'perfectionist/sort-exports': 'error', // export 정렬
+      'perfectionist/sort-named-exports': 'error', // 이름 지정 export 정렬
+    },
+  },
 );
